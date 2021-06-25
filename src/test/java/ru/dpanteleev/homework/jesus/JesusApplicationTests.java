11e52ffee1040748af7eed5cbb04774792172e8c
dmitry <123>
2021-06-25 17:37:14 +0300

    Initial commit

---
+++ src/test/java/ru/dpanteleev/homework/jesus/JesusApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dpanteleev.homework.jesus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JesusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
